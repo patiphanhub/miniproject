0b741d6d7bc314d40efa111979e189cca95ae0a1
patiphanhub <patipan14534@gmail.com>
2024-10-16 03:43:36 +0700

    Delete system directory

--- system/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();  // ลบข้อมูลในเซสชันทั้งหมด
-session_destroy();  // ทำลายเซสชัน
-
-// เปลี่ยนหน้ากลับไปที่หน้า login
-header("Location: login.php");
-exit();
-?>
